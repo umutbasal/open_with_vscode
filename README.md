840317766c848e5a760f7fb9961842b93b98b964
umutbasal <umut.bsl@yandex.com>
2022-01-19 12:11:56 +0300

    inject elements to repo page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Open with VS Code Extension
+# Open with VS Code Chrome Extension
 this extension allows you to open github repos in vscode with remote container (local codespace), strict read mode and vscode web app options.
+
+# Install
+clone it to your local and load it as unpacked extension on `chrome://extensions` page
--- open-with-vscode.js
+++ open-with-vscode.js
@@ -1,2 +1,59 @@
-const gitURL = document.querySelector("#local-panel > ul > li:nth-child(1) > tab-container > div:nth-child(2) > div > input").value;
-console.log(gitURL);
+window.addEventListener('DOMContentLoaded', (event) => {
+	// check if the page is repo page
+	if (document.querySelector("#command-palette-pjax-meta-data").getAttribute('data-subject-type') == 'Repository') {
+
+		// hold junk element to prevent destroy existing events of the old elements
+		let junkElement = document.createElement("div");
+
+		// Create the tab
+		const tabs = document.querySelector("#repo-content-pjax-container get-repo ul");
+		junkElement.innerHTML = `
+		<li role="presentation" data-view-component="true" class="hx_tabnav-in-dropdown-wrapper flex-1 d-inline-flex">
+			<button data-tab="vscode" data-action="click:get-repo#vscodeTabSelected focusin:get-repo#vscodeTabSelected" id="vscode-tab" type="button" role="tab" aria-controls="vscode-panel" aria-selected="false" data-view-component="true" class="tabnav-tab flex-1" tabindex="-1">
+				<span data-view-component="true">VS Code</span>
+			</button>
+		</li>`;
+		tabs.appendChild(junkElement.firstElementChild);
+
+		// Create the container
+		const tabContainer = document.querySelector("#repo-content-pjax-container get-repo feature-callout tab-container");
+		junkElement.innerHTML = `
+		<div id="vscode-panel" role="tabpanel" tabindex="0" aria-labelledby="vscode-tab" data-view-component="true">
+			<ul class="list-style-none">
+				<li class="Box-row Box-row--hover-gray p-3 mt-0">
+					<a class="d-flex flex-items-center color-fg-default text-bold no-underline" rel="nofollow"
+						id="open_with_vscode_opt_1"
+						href="#">
+						Read on VS Code
+					</a>
+				</li>
+				<li class="Box-row Box-row--hover-gray p-3 mt-0">
+					<a class="d-flex flex-items-center color-fg-default text-bold no-underline" rel="nofollow"
+						id="open_with_vscode_opt_2"
+						href="#">
+						Clone to docker with Remote Containers
+					</a>
+				</li>
+				<li class="Box-row Box-row--hover-gray p-3 mt-0">
+					<a class="d-flex flex-items-center color-fg-default text-bold no-underline" rel="nofollow"
+						id="open_with_vscode_opt_3"
+						href="#">
+						Read on Browser
+					</a>
+				</li>
+			</ul>
+		</div>`;
+		tabContainer.appendChild(junkElement.firstElementChild);
+
+		// Listen for clicks
+		document.querySelector("#open_with_vscode_opt_1").addEventListener("click", () => {
+			window.location.href = `vscode://github.remotehub/open?url%3D${location.href}`
+		});
+		document.querySelector("#open_with_vscode_opt_2").addEventListener("click", () => {
+			window.location.href = `vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=${location.href}`
+		});
+		document.querySelector("#open_with_vscode_opt_3").addEventListener("click", () => {
+			window.location.href = `https://vscode.dev/${location.href}`
+		});
+	}
+});
