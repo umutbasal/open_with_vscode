d525fbc1693880e58d824072dd1407e33f6d1ad0
umutbasal <umut.bsl@yandex.com>
2022-01-19 12:13:02 +0300

    dont wait for dom load

--- open-with-vscode.js
+++ open-with-vscode.js
@@ -1,23 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-	// check if the page is repo page
-	if (document.querySelector("#command-palette-pjax-meta-data").getAttribute('data-subject-type') == 'Repository') {
+// check if the page is repo page
+if (document.querySelector("#command-palette-pjax-meta-data").getAttribute('data-subject-type') == 'Repository') {
 
-		// hold junk element to prevent destroy existing events of the old elements
-		let junkElement = document.createElement("div");
+	// hold junk element to prevent destroy existing events of the old elements
+	let junkElement = document.createElement("div");
 
-		// Create the tab
-		const tabs = document.querySelector("#repo-content-pjax-container get-repo ul");
-		junkElement.innerHTML = `
+	// Create the tab
+	const tabs = document.querySelector("#repo-content-pjax-container get-repo ul");
+	junkElement.innerHTML = `
 		<li role="presentation" data-view-component="true" class="hx_tabnav-in-dropdown-wrapper flex-1 d-inline-flex">
 			<button data-tab="vscode" data-action="click:get-repo#vscodeTabSelected focusin:get-repo#vscodeTabSelected" id="vscode-tab" type="button" role="tab" aria-controls="vscode-panel" aria-selected="false" data-view-component="true" class="tabnav-tab flex-1" tabindex="-1">
 				<span data-view-component="true">VS Code</span>
 			</button>
 		</li>`;
-		tabs.appendChild(junkElement.firstElementChild);
+	tabs.appendChild(junkElement.firstElementChild);
 
-		// Create the container
-		const tabContainer = document.querySelector("#repo-content-pjax-container get-repo feature-callout tab-container");
-		junkElement.innerHTML = `
+	// Create the container
+	const tabContainer = document.querySelector("#repo-content-pjax-container get-repo feature-callout tab-container");
+	junkElement.innerHTML = `
 		<div id="vscode-panel" role="tabpanel" tabindex="0" aria-labelledby="vscode-tab" data-view-component="true">
 			<ul class="list-style-none">
 				<li class="Box-row Box-row--hover-gray p-3 mt-0">
@@ -43,17 +42,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
 				</li>
 			</ul>
 		</div>`;
-		tabContainer.appendChild(junkElement.firstElementChild);
+	tabContainer.appendChild(junkElement.firstElementChild);
 
-		// Listen for clicks
-		document.querySelector("#open_with_vscode_opt_1").addEventListener("click", () => {
-			window.location.href = `vscode://github.remotehub/open?url%3D${location.href}`
-		});
-		document.querySelector("#open_with_vscode_opt_2").addEventListener("click", () => {
-			window.location.href = `vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=${location.href}`
-		});
-		document.querySelector("#open_with_vscode_opt_3").addEventListener("click", () => {
-			window.location.href = `https://vscode.dev/${location.href}`
-		});
-	}
-});
+	// Listen for clicks
+	document.querySelector("#open_with_vscode_opt_1").addEventListener("click", () => {
+		window.location.href = `vscode://github.remotehub/open?url%3D${location.href}`
+	});
+	document.querySelector("#open_with_vscode_opt_2").addEventListener("click", () => {
+		window.location.href = `vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=${location.href}`
+	});
+	document.querySelector("#open_with_vscode_opt_3").addEventListener("click", () => {
+		window.location.href = `https://vscode.dev/${location.href}`
+	});
+}
